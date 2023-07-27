84e006eb2d0a6279bd7992065dd6090e7e83dd07
Carlos Miguel Castro <castro20020308@gmail.com>
2023-07-27 11:05:38 +0800

    update app

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import "../App.css";
 
 /* bootstrap icons imports */
 import { IconContext } from "react-icons";
-import { BsFillPersonFill, BsFillCartFill } from "react-icons/bs";
+import { BsFillPersonFill, BsFillCartFill, BsFillPlusCircleFill } from "react-icons/bs";
 
 function Home() {
     return (
@@ -16,21 +16,40 @@ function Home() {
                         <BsFillPersonFill />
                     </IconContext.Provider>
                 </div>
-                <div className="potato">
-                        <div className="home-container-contents">
-                            <div className="home-container-contents-images">
-                            <div className="Lubing"><img src="Lubing.jpg" alt="Lubing" width="350" height="350"></img></div>
-                            <div className="Case-Foam"><img src="Case Foam.jpg" alt="Case Foam" width="350" height="350"></img></div>
-                                
-                                
-                            <div className="Stabilizer Tuning"><img src="Stabilizer Tuning.jpg" alt="Stabilizer Tuning" width="350" height="350"></img></div>
-                            <div className="Tape Mod"><img src="Tape Mod.jpg" alt="Tape Mod" width="350" height="350"></img></div>
-                                
-                                
-                            <div className="PE-Foam"><img src="pe foam.jpg" alt="PE Foam" width="350" height="350"></img></div>
-                            <div className="Films"><img src="films.jpg" alt="Films" width="350" height="350"></img></div>
-                                
+                
+                <div className="home-container-contents">
+                    <div className="home-container-contents-images">
+                        <div className="Lubing"><img src="Lubing.jpg" alt="Lubing" width="330" height="330" />
+                            <div className="Lubing-contents">
+                                <p>Switch Lubing</p>
+                                <div className="Lubing-icons">
+                                    <IconContext.Provider value={{ size: '20px', stroke: "black" }}>
+                                        <BsFillPlusCircleFill />
+                                    </IconContext.Provider>
+                                </div>
+                            </div>
+                            <h4>PHP 8/Switch</h4>
                         </div>
+                        <div className="Case-Foam"><img src="Case Foam.jpg" alt="Case Foam" width="330" height="330" />
+                            <p>Case Foam</p>
+                            <h4>PHP 50</h4>
+                        </div>     
+                        <div className="Stabilizer-Tuning"><img src="Stabilizer Tuning.jpg" alt="Stabilizer Tuning" width="330" height="330" />
+                            <p>Stabilizer Tuning</p>
+                            <h4>PHP 50/Stabilizer</h4>
+                        </div>
+                        <div className="Tape-Mod"><img src="Tape Mod.jpg" alt="Tape Mod" width="330" height="330" />
+                            <p>Tape Mod</p>
+                            <h4>PHP 10/Layer</h4>
+                        </div>     
+                        <div className="PE-Foam"><img src="pe foam.jpg" alt="PE Foam" width="330" height="330" />
+                            <p>PE Foam Mod</p>
+                            <h4>PHP 50</h4>
+                        </div>
+                        <div className="Films"><img src="films.jpg" alt="Films" width="330" height="330" />
+                            <p>Filming</p>
+                            <h4>PHP 6/Switch</h4>
+                        </div>   
                     </div>
                 </div>
             </div>
